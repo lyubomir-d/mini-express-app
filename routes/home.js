31cec289140303723f277772f2740c78e9165afc
Lyubomir <lubomir@topnlab.ru>
2020-06-23 17:31:03 +0300

    Added nodemailer and send mail feature

--- routes/home.js
+++ routes/home.js
@@ -34,10 +34,40 @@ let cpUpload = upload.fields([
     { name: 'car_registration' },
 ])
 
+/*** Send mail ***/
+const nodemailer = require("nodemailer");
+
+async function mail(textMail) {
+    let testEmailAccount = await
+    nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: testEmailAccount.user,
+            pass: testEmailAccount.pass
+        }
+    });
+
+    let result = await
+    transporter.sendMail({
+        from: '"Node js" <nodejs@example.com>',
+        to: "ruldo@bk.ru",
+        subject: "Message from Node js",
+        text: "This message was sent from Node js server.",
+        html: "This <i>message</i> was sent from <strong>Node js</strong> server."
+    });
+    console.log(result);
+}
+
+/*** end of send mail ***/
+
 router.post('/upload', cpUpload, function (req, res, next) {
     let files = req.files;
-    let error = '';
-    console.log(files);
+    console.log('files', files);
+    mail("Приветик").catch(console.error);
     if(!files)
         status = "Ошибка при загрузке файла";
     else
